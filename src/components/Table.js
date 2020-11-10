4a195e7201feb148bd39ffd6528a26f0426a3dd2
Ramalakshmi Lakshmanan <ramalk0813@gmail.com>
2020-11-10 15:04:58 -0500

    react covid-app

---
+++ src/components/Table.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "../App.css";
+//import numeral from "numeral";
+
+function Table({ countries }) {
+  return (
+    <div className="table">
+      {countries.map((country) => (
+        <tr>
+          <td>{country.country}</td>
+          <td>
+           {/* <strong>{numeral(country.cases).format("0,0")}</strong>*/}
+           <strong>{country.cases}</strong>
+          </td>
+        </tr>
+      ))}
+    </div>
+  );
+}
+
+export default Table;
